9c66881c0f5b262dc81a2807d8c8955af7026202
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-23 20:09:56 -0700

    remove poet node specific code and interfaces (#67)

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -56,17 +56,3 @@ export interface PoetTimestamp extends TransactionPoetTimestamp {
   readonly blockHeight: number
   readonly blockHash: string
 }
-
-export interface ClaimIPFSHashPair {
-  readonly claim: Claim
-  readonly ipfsHash: string
-}
-
-export function isClaimIPFSHashPair(o: any): o is ClaimIPFSHashPair {
-  return o.claim && isClaim(o.claim) && o.ipfsHash
-}
-
-export interface ClaimIdIPFSHashPair {
-  readonly claimId: string
-  readonly ipfsHash: string
-}
