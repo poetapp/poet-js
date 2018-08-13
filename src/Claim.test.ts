fc1f886cd0d26b3c66c806f2d5dd96cced650936
Kris Robison <krobison@gmail.com>
2018-08-13 11:52:53 -0700

    Modify getClaimId to use jsonld canonization
    Note that this is a promise-based method as it has to load the context schema. Propogated the promise processing down, and a similar pull request will be opened in FrostApi to continue the promise handling.

--- src/Claim.test.ts
+++ src/Claim.test.ts
@@ -1,6 +1,13 @@
 /* tslint:disable:no-relative-imports */
 import { describe } from 'riteway'
-import { createClaim, isValidSignature, getClaimId, getClaimSignature, isValidClaim } from './Claim'
+import {
+  createClaim,
+  isValidSignature,
+  getClaimId,
+  getClaimSignature,
+  isValidClaim,
+  expandAndCanonizeClaim,
+} from './Claim'
 import { Claim, ClaimType, ClaimAttributes, Work } from './Interfaces'
 
 const makeClaim = (attributes: ClaimAttributes) => {
@@ -16,10 +23,10 @@ const makeClaim = (attributes: ClaimAttributes) => {
 }
 
 const TheRaven: Work = {
-  id: '1bb5e7959c7cb28936ec93eb6893094241a5bc396f08845b4f52c86034f0ddf8',
+  id: '58198ffd43c435bcdbb811c79f03839425174b3bdb11d02f105f238152f4616b',
   publicKey: '02badf4650ba545608242c2d303d587cf4f778ae3cf2b3ef99fbda37555a400fd2',
   signature:
-    '3045022100e020a7ffeffa5d40ffde618c6c861678e38de69fd377028ec57ad93893883b3702201f085284a9064bab7e1cd39349e65d136d8f67e4b6b897c3e7db6b400ed91034',
+    '3045022100ed4d29e235e418c47ababb554156d11fbd09781e309805d6f4f876e8a4c5c3c702200b52e7ed96b540f5c64d5746db3ba4b34b5448404876f98d834623b25220d242',
   type: ClaimType.Work,
   dateCreated: new Date('2017-11-13T15:00:00.000Z'),
   attributes: {
@@ -32,6 +39,12 @@ const TheRaven: Work = {
   },
 }
 
+const expectedCanonicalDoc =
+  '_:c14n0 <http://schema.org/CreativeWork> _:c14n1 .\n' +
+  '_:c14n0 <http://schema.org/Text> "02db393ae2d566ceddd95a97fd88bc2897a0818528158261cec45087a58786f09d" .\n' +
+  '_:c14n0 <http://schema.org/additionalType> "Work" .\n' +
+  '_:c14n0 <http://schema.org/dateCreated> "Mon Dec 11 2017 14:54:40 GMT-0800 (PST)" .\n'
+
 const Key = {
   privateKey: 'L1mptZyB6aWkiJU7dvAK4UUjLSaqzcRNYJn3KuAA7oEVyiNn3ZPF',
   publicKey: '02cab54b227f16dd4866310799842cdd239f2adb56d0a3789519c6f43a892a61f6',
@@ -41,31 +54,117 @@ const PrivateKeyEAP = 'KxuZJmgVAipi9hfYXHTyGYmmhkbG7fBzmkyVnj6t9j9rDR1nN1vN'
 
 describe('Claim', async (should: any) => {
   const { assert } = should('')
+  const returnError = (err: Error) => err
 
   {
-    const claim = createClaim(Key.privateKey, ClaimType.Work, TheRaven.attributes)
+    const claim = makeClaim({
+      name: TheRaven.attributes.name,
+      author: TheRaven.attributes.author,
+    })
+
+    const canonicalDoc = await expandAndCanonizeClaim(claim).catch(returnError)
 
     assert({
-      given: 'the public key of a Claim',
-      should: 'be equal to public key of key',
-      actual: claim.publicKey,
-      expected: Key.publicKey,
+      given: 'a claim',
+      should: 'create a canonical string document',
+      actual: canonicalDoc,
+      expected: expectedCanonicalDoc,
     })
   }
 
   {
-    const claim = createClaim(Key.privateKey, ClaimType.Work, TheRaven.attributes)
+    const work1: Claim = makeClaim({
+      name: TheRaven.attributes.name,
+      author: TheRaven.attributes.author,
+    })
+
+    const work2: Claim = makeClaim({
+      author: TheRaven.attributes.author,
+      name: TheRaven.attributes.name,
+    })
+
+    const canonicalDocument1 = await expandAndCanonizeClaim(work1).catch(returnError)
+    const canonicalDocument2 = await expandAndCanonizeClaim(work2).catch(returnError)
 
     assert({
-      given: 'the result of isValidSignature of Claim with a valid signature',
-      should: 'should return true',
-      actual: isValidSignature(claim),
+      given: 'two claims with disordered keys',
+      should: 'have the same canonical document',
+      actual: canonicalDocument1 === canonicalDocument2,
       expected: true,
     })
   }
 
   {
-    const claimId = getClaimId(TheRaven)
+    const work1: Claim = makeClaim({
+      name: TheRaven.attributes.name,
+      author: TheRaven.attributes.author,
+    })
+
+    const work2: Claim = makeClaim({
+      Author: TheRaven.attributes.author,
+      NAME: TheRaven.attributes.name,
+    })
+
+    const canonicalDocument1 = await expandAndCanonizeClaim(work1).catch(returnError)
+    const canonicalDocument2 = await expandAndCanonizeClaim(work2).catch(returnError)
+
+    assert({
+      given: 'two claims with keys casing',
+      should: 'have the same canonical document',
+      actual: canonicalDocument1 === canonicalDocument2,
+      expected: true,
+    })
+  }
+
+  {
+    const claim = makeClaim({
+      name: TheRaven.attributes.name,
+      author: TheRaven.attributes.author,
+    })
+
+    const id = await getClaimId(claim)
+
+    assert({
+      given: 'A claim',
+      should: 'generate an id for the claim',
+      actual: id,
+      expected: '31f4dd63047c5a053aead0dbb9dd24a08b2b97c153f7f80e03d839371f558b32',
+    })
+  }
+
+  {
+    try {
+      const claim = await createClaim(Key.privateKey, ClaimType.Work, TheRaven.attributes)
+
+      assert({
+        given: 'the public key of a Claim',
+        should: 'be equal to public key of key',
+        actual: claim.publicKey,
+        expected: Key.publicKey,
+      })
+    } catch (e) {
+      throw e
+    }
+  }
+
+  {
+    try {
+      const claim = await createClaim(Key.privateKey, ClaimType.Work, TheRaven.attributes)
+      const isValid = await isValidSignature(claim)
+
+      assert({
+        given: 'the result of isValidSignature of Claim with a valid signature',
+        should: 'should return true',
+        actual: isValid,
+        expected: true,
+      })
+    } catch (e) {
+      throw e
+    }
+  }
+
+  {
+    const claimId = await getClaimId(TheRaven).catch(returnError)
 
     assert({
       given: 'a claim id',
@@ -76,7 +175,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = getClaimId({ ...TheRaven, id: '123' })
+    const claimId = await getClaimId({ ...TheRaven, id: '123' }).catch(returnError)
 
     assert({
       given: 'a claim with extra id, the new id',
@@ -87,7 +186,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = getClaimId({ ...TheRaven, signature: '123' })
+    const claimId = await getClaimId({ ...TheRaven, signature: '123' }).catch(returnError)
 
     assert({
       given: 'a claim with extra signature, the new signature',
@@ -98,7 +197,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = getClaimId({ ...TheRaven, publicKey: '123' })
+    const claimId = await getClaimId({ ...TheRaven, publicKey: '123' }).catch(returnError)
 
     assert({
       given: 'a claim with extra publicKey, the new publicKey',
@@ -109,7 +208,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = getClaimId({ ...TheRaven, type: 'Asd' as ClaimType })
+    const claimId = await getClaimId({ ...TheRaven, type: 'Asd' as ClaimType }).catch(returnError)
 
     assert({
       given: 'a claim with extra type, the new type',
@@ -120,7 +219,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = getClaimId({ ...TheRaven, dateCreated: new Date() })
+    const claimId = await getClaimId({ ...TheRaven, dateCreated: new Date() }).catch(returnError)
 
     assert({
       given: 'a claim with extra dateCreated, the new dateCreated',
@@ -141,8 +240,8 @@ describe('Claim', async (should: any) => {
       name: TheRaven.attributes.name,
     })
 
-    const claimId1 = getClaimId(work1)
-    const claimId2 = getClaimId(work2)
+    const claimId1 = await getClaimId(work1).catch(returnError)
+    const claimId2 = await getClaimId(work2).catch(returnError)
 
     assert({
       given: 'two claims with disordered keys',
@@ -163,8 +262,8 @@ describe('Claim', async (should: any) => {
       NAME: TheRaven.attributes.name,
     })
 
-    const claimId1 = getClaimId(work1)
-    const claimId2 = getClaimId(work2)
+    const claimId1 = await getClaimId(work1).catch(returnError)
+    const claimId2 = await getClaimId(work2).catch(returnError)
 
     assert({
       given: 'two claims with keys casing',
@@ -175,21 +274,25 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const signature = getClaimSignature(TheRaven, PrivateKeyEAP)
+    try {
+      const signature = await getClaimSignature(TheRaven, PrivateKeyEAP)
 
-    assert({
-      given: 'a signature of a Claim',
-      should: 'be the signature equal to of work signature',
-      actual: signature,
-      expected: TheRaven.signature,
-    })
+      assert({
+        given: 'a signature of a Claim',
+        should: 'be the signature equal to of work signature',
+        actual: signature,
+        expected: TheRaven.signature,
+      })
+    } catch (e) {
+      throw e
+    }
   }
 
   {
     const expected = 'Cannot sign a claim that has an empty .id field.'
 
     try {
-      getClaimSignature({ ...TheRaven, id: '' }, PrivateKeyEAP)
+      await getClaimSignature({ ...TheRaven, id: '' }, PrivateKeyEAP)
     } catch (e) {
       assert({
         given: 'a claim without id',
@@ -204,7 +307,7 @@ describe('Claim', async (should: any) => {
     const expected = 'Cannot sign a claim whose id has been altered or generated incorrectly.'
 
     try {
-      getClaimSignature(
+      await getClaimSignature(
         { ...TheRaven, id: 'be81cc75bcf6ca0f1fdd356f460e6ec920ba36ec78bd9e70c4d04a19f8943102' },
         PrivateKeyEAP
       )
@@ -222,7 +325,7 @@ describe('Claim', async (should: any) => {
     const expected = 'Cannot sign a claim that has an empty .publicKey field.'
 
     try {
-      getClaimSignature({ ...TheRaven, publicKey: undefined }, PrivateKeyEAP)
+      await getClaimSignature({ ...TheRaven, publicKey: undefined }, PrivateKeyEAP)
     } catch (e) {
       assert({
         given: 'a claim with publicKey undefined',
@@ -237,7 +340,7 @@ describe('Claim', async (should: any) => {
     const expected = `Cannot sign this claim with the provided privateKey. It doesn\t match the claim's public key.`
 
     try {
-      getClaimSignature(
+      await getClaimSignature(
         { ...TheRaven, publicKey: '03f0dc475e93105bdc7701b40003200039202ffd4a0789696c78f9b34d5518aef9' },
         PrivateKeyEAP
       )
@@ -255,7 +358,7 @@ describe('Claim', async (should: any) => {
     assert({
       given: 'a valid claim, isValidClaim',
       should: `return true`,
-      actual: isValidClaim(TheRaven),
+      actual: await isValidClaim(TheRaven),
       expected: true,
     })
   }
@@ -264,7 +367,7 @@ describe('Claim', async (should: any) => {
     assert({
       given: 'an object that is not a claim',
       should: `return false`,
-      actual: isValidClaim({ foo: 'bar' }),
+      actual: await isValidClaim({ foo: 'bar' }),
       expected: false,
     })
   }
@@ -273,7 +376,7 @@ describe('Claim', async (should: any) => {
     assert({
       given: 'a claim with an invalid id',
       should: `return false`,
-      actual: isValidClaim({ ...TheRaven, id: '111' }),
+      actual: await isValidClaim({ ...TheRaven, id: '111' }),
       expected: false,
     })
   }
@@ -282,7 +385,7 @@ describe('Claim', async (should: any) => {
     assert({
       given: 'a claim with an invalid publicKey',
       should: `return false`,
-      actual: isValidClaim({ ...TheRaven, publicKey: '111' }),
+      actual: await isValidClaim({ ...TheRaven, publicKey: '111' }),
       expected: false,
     })
   }
@@ -291,19 +394,21 @@ describe('Claim', async (should: any) => {
     assert({
       given: 'a claim with an invalid signature',
       should: `return false`,
-      actual: isValidClaim({ ...TheRaven, signature: '111' }),
+      actual: await isValidClaim({ ...TheRaven, signature: '111' }),
       expected: false,
     })
   }
 
-  ;['', false, null, undefined].forEach(value => {
-    {
-      assert({
-        given: 'a claim with an invalid date',
-        should: `return false`,
-        actual: isValidClaim({ ...TheRaven, dateCreated: value }),
-        expected: false,
-      })
-    }
-  })
+  {
+    const result = await Promise.all(
+      ['', false, null, undefined].map(value => isValidClaim({ ...TheRaven, dateCreated: value }))
+    )
+
+    assert({
+      given: 'a claim with an invalid input',
+      should: `return false`,
+      actual: result,
+      expected: [false, false, false, false],
+    })
+  }
 })
--- src/Claim.ts
+++ src/Claim.ts
@@ -1,42 +1,53 @@
 /* tslint:disable:no-relative-imports */
 import * as bitcore from 'bitcore-lib'
 import * as crypto from 'crypto'
+import { expand, canonize } from 'jsonld'
 
 import { IllegalArgumentException } from './Exceptions'
-import { Claim, ClaimAttributes, ClaimType, isClaim } from './Interfaces'
-import { Serialization } from './Serialization'
+import { Claim, ClaimAttributes, ClaimContext, ClaimType, isClaim } from './Interfaces'
 
-export function getClaimId(claim: Claim): string {
-  const buffer = Buffer.from(
-    Serialization.claimToHex({
-      ...claim,
-      id: '',
-      signature: '',
-    }),
-    'hex'
-  )
-  return crypto
-    .createHash('sha256')
-    .update(buffer)
-    .digest()
-    .toString('hex')
+const claimContex: ClaimContext = {
+  publicKey: 'http://schema.org/Text',
+  dateCreated: 'http://schema.org/dateCreated',
+  type: 'http://schema.org/additionalType',
+  attributes: 'http://schema.org/CreativeWork',
 }
 
-export function getClaimSignature(claim: Claim, privateKey: string): string {
+export const expandAndCanonizeClaim = async (claim: Claim): Promise<string> => {
+  const contextualClaim = { '@context': claimContex, ...claim }
+  return await canonize(await expand(contextualClaim))
+}
+
+export const getClaimId = async (claim: Claim): Promise<string> => {
+  try {
+    const canonizedClaim = await expandAndCanonizeClaim(claim)
+    const buffer = Buffer.from(canonizedClaim)
+    return crypto
+      .createHash('sha256')
+      .update(buffer)
+      .digest()
+      .toString('hex')
+  } catch (e) {
+    throw e
+  }
+}
+
+export const getClaimSignature = async (claim: Claim, privateKey: string): Promise<string> => {
   if (!claim.publicKey) throw new IllegalArgumentException('Cannot sign a claim that has an empty .publicKey field.')
   if (new bitcore.PrivateKey(privateKey).publicKey.toString() !== claim.publicKey)
     throw new IllegalArgumentException(
       "Cannot sign this claim with the provided privateKey. It doesn\t match the claim's public key."
     )
+  const generatedClaimId = await getClaimId(claim)
   if (!claim.id) throw new IllegalArgumentException('Cannot sign a claim that has an empty .id field.')
-  if (claim.id !== getClaimId(claim))
+  if (claim.id !== generatedClaimId)
     throw new IllegalArgumentException('Cannot sign a claim whose id has been altered or generated incorrectly.')
 
   const signature = bitcore.crypto.ECDSA.sign(Buffer.from(claim.id, 'hex'), new bitcore.PrivateKey(privateKey))
   return signature.toString()
 }
 
-export function isValidSignature(claim: Claim): boolean {
+export const isValidSignature = async (claim: Claim): Promise<boolean> => {
   try {
     return bitcore.crypto.ECDSA.verify(
       Buffer.from(claim.id, 'hex'),
@@ -48,7 +59,7 @@ export function isValidSignature(claim: Claim): boolean {
   }
 }
 
-export function createClaim(privateKey: string, type: ClaimType, attributes: ClaimAttributes): Claim {
+export const createClaim = async (privateKey: string, type: ClaimType, attributes: ClaimAttributes): Promise<Claim> => {
   const claim: Claim = {
     id: '',
     publicKey: new bitcore.PrivateKey(privateKey).publicKey.toString(),
@@ -57,20 +68,24 @@ export function createClaim(privateKey: string, type: ClaimType, attributes: Cla
     dateCreated: new Date(),
     attributes,
   }
-  const id = getClaimId(claim)
-  const signature = getClaimSignature(
-    {
+  const id = await getClaimId(claim)
+  try {
+    const signature = await getClaimSignature(
+      {
+        ...claim,
+        id,
+      },
+      privateKey
+    )
+    return {
       ...claim,
       id,
-    },
-    privateKey
-  )
-  return {
-    ...claim,
-    id,
-    signature,
+      signature,
+    }
+  } catch (e) {
+    throw e
   }
 }
 
-export const isValidClaim = (claim = {}): boolean =>
-  !!isClaim(claim) && isValidSignature(claim) && getClaimId(claim) === claim.id
+export const isValidClaim = async (claim = {}): Promise<boolean> =>
+  !!isClaim(claim) && (await isValidSignature(claim)) && (await getClaimId(claim)) === claim.id
