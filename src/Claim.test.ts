8e7af033410669ce6d134b9ef91edfd405d757d7
Kris Robison <krobison@gmail.com>
2018-08-14 13:22:47 -0700

    Modify values to address type definitions
    Clean tests up further

--- src/Claim.test.ts
+++ src/Claim.test.ts
@@ -11,39 +11,53 @@ import {
 import { Claim, ClaimType, ClaimAttributes, Work } from './Interfaces'
 
 const makeClaim = (attributes: ClaimAttributes) => {
-  const dateCreated = new Date('2017-12-11T22:54:40.261Z')
+  const created = new Date('2017-12-11T22:54:40.261Z')
   const publicKey = '02db393ae2d566ceddd95a97fd88bc2897a0818528158261cec45087a58786f09d'
   const type = ClaimType.Work
   return {
     publicKey,
-    dateCreated,
+    created,
     type,
     attributes,
   }
 }
 
 const TheRaven: Work = {
-  id: '58198ffd43c435bcdbb811c79f03839425174b3bdb11d02f105f238152f4616b',
+  id: '86542f4ec9dfb6463631f0a759edba3c73301dd9858b02f3239d176d73fbb246',
   publicKey: '02badf4650ba545608242c2d303d587cf4f778ae3cf2b3ef99fbda37555a400fd2',
   signature:
-    '3045022100ed4d29e235e418c47ababb554156d11fbd09781e309805d6f4f876e8a4c5c3c702200b52e7ed96b540f5c64d5746db3ba4b34b5448404876f98d834623b25220d242',
+    '30450221008924492f1888eba787ca97fb57dc62521a5dafecb486a71a7f745bf56174792502204e0b7ce778c4fc5a2995fe4a97cf9d0e0886573cdc6334520a74c68b3d47e5fd',
   type: ClaimType.Work,
-  dateCreated: new Date('2017-11-13T15:00:00.000Z'),
+  created: new Date('2017-11-13T15:00:00.000Z'),
   attributes: {
     name: 'The Raven',
     author: 'Edgar Allan Poe',
     tags: 'poem',
     dateCreated: '',
     datePublished: '1845-01-29T03:00:00.000Z',
-    content: 'Once upon a midnight dreary...',
+    text: 'Once upon a midnight dreary...',
   },
 }
 
+const canonicalRaven =
+  '_:c14n0 <http://schema.org/author> "Edgar Allan Poe" .\n' +
+  '_:c14n0 <http://schema.org/dateCreated> "" .\n' +
+  '_:c14n0 <http://schema.org/datePublished> "1845-01-29T03:00:00.000Z" .\n' +
+  '_:c14n0 <http://schema.org/keyword> "poem" .\n' +
+  '_:c14n0 <http://schema.org/name> "The Raven" .\n' +
+  '_:c14n0 <http://schema.org/text> "Once upon a midnight dreary..." .\n' +
+  '_:c14n1 <http://purl.org/dcterms/created> "Mon Nov 13 2017 07:00:00 GMT-0800 (PST)" .\n' +
+  '_:c14n1 <http://schema.org/CreativeWork> _:c14n0 .\n' +
+  '_:c14n1 <http://schema.org/Text> "02badf4650ba545608242c2d303d587cf4f778ae3cf2b3ef99fbda37555a400fd2" .\n' +
+  '_:c14n1 <http://schema.org/additionalType> "Work" .\n'
+
 const expectedCanonicalDoc =
+  '_:c14n0 <http://purl.org/dcterms/created> "Mon Dec 11 2017 14:54:40 GMT-0800 (PST)" .\n' +
   '_:c14n0 <http://schema.org/CreativeWork> _:c14n1 .\n' +
   '_:c14n0 <http://schema.org/Text> "02db393ae2d566ceddd95a97fd88bc2897a0818528158261cec45087a58786f09d" .\n' +
   '_:c14n0 <http://schema.org/additionalType> "Work" .\n' +
-  '_:c14n0 <http://schema.org/dateCreated> "Mon Dec 11 2017 14:54:40 GMT-0800 (PST)" .\n'
+  '_:c14n1 <http://schema.org/author> "Edgar Allan Poe" .\n' +
+  '_:c14n1 <http://schema.org/name> "The Raven" .\n'
 
 const Key = {
   privateKey: 'L1mptZyB6aWkiJU7dvAK4UUjLSaqzcRNYJn3KuAA7oEVyiNn3ZPF',
@@ -54,7 +68,16 @@ const PrivateKeyEAP = 'KxuZJmgVAipi9hfYXHTyGYmmhkbG7fBzmkyVnj6t9j9rDR1nN1vN'
 
 describe('Claim', async (should: any) => {
   const { assert } = should('')
-  const returnError = (err: Error) => err
+  const returnError = (err: Error): Error => err
+
+  {
+    assert({
+      given: 'a complete claim',
+      should: 'include all items in canonical doc',
+      actual: await expandAndCanonizeClaim(TheRaven),
+      expected: canonicalRaven,
+    })
+  }
 
   {
     const claim = makeClaim({
@@ -62,12 +85,10 @@ describe('Claim', async (should: any) => {
       author: TheRaven.attributes.author,
     })
 
-    const canonicalDoc = await expandAndCanonizeClaim(claim).catch(returnError)
-
     assert({
       given: 'a claim',
       should: 'create a canonical string document',
-      actual: canonicalDoc,
+      actual: await expandAndCanonizeClaim(claim).catch(returnError),
       expected: expectedCanonicalDoc,
     })
   }
@@ -110,8 +131,8 @@ describe('Claim', async (should: any) => {
 
     assert({
       given: 'two claims with keys casing',
-      should: 'have the same canonical document',
-      actual: canonicalDocument1 === canonicalDocument2,
+      should: 'NOT have the same canonical document',
+      actual: canonicalDocument1 !== canonicalDocument2,
       expected: true,
     })
   }
@@ -122,13 +143,11 @@ describe('Claim', async (should: any) => {
       author: TheRaven.attributes.author,
     })
 
-    const id = await getClaimId(claim)
-
     assert({
       given: 'A claim',
       should: 'generate an id for the claim',
-      actual: id,
-      expected: '31f4dd63047c5a053aead0dbb9dd24a08b2b97c153f7f80e03d839371f558b32',
+      actual: await getClaimId(claim),
+      expected: 'a633a579c4ee656dfeb9fceae15643681dbc957dac910dcd346458ab555c832e',
     })
   }
 
@@ -156,34 +175,28 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = await getClaimId(TheRaven).catch(returnError)
-
     assert({
       given: 'a claim id',
       should: 'be equal to the work id',
-      actual: claimId,
+      actual: await getClaimId(TheRaven).catch(returnError),
       expected: TheRaven.id,
     })
   }
 
   {
-    const claimId = await getClaimId({ ...TheRaven, id: '123' }).catch(returnError)
-
     assert({
       given: 'a claim with extra id, the new id',
       should: 'be ignored in the calculation of the id and should be equal to the work id',
-      actual: claimId,
+      actual: await getClaimId({ ...TheRaven, id: '123' }).catch(returnError),
       expected: TheRaven.id,
     })
   }
 
   {
-    const claimId = await getClaimId({ ...TheRaven, signature: '123' }).catch(returnError)
-
     assert({
       given: 'a claim with extra signature, the new signature',
       should: 'be ignored in the calculation of the id and should be equal to the work id',
-      actual: claimId,
+      actual: await getClaimId({ ...TheRaven, signature: '123' }).catch(returnError),
       expected: TheRaven.id,
     })
   }
@@ -211,7 +224,7 @@ describe('Claim', async (should: any) => {
   }
 
   {
-    const claimId = await getClaimId({ ...TheRaven, dateCreated: new Date() }).catch(returnError)
+    const claimId = await getClaimId({ ...TheRaven, created: new Date() }).catch(returnError)
 
     assert({
       given: 'a claim with extra dateCreated, the new dateCreated',
@@ -259,8 +272,8 @@ describe('Claim', async (should: any) => {
 
     assert({
       given: 'two claims with keys casing',
-      should: 'have the same claims id',
-      actual: claimId1 === claimId2,
+      should: 'NOT have the same claims id',
+      actual: claimId1 !== claimId2,
       expected: true,
     })
   }
@@ -308,7 +321,7 @@ describe('Claim', async (should: any) => {
       given: 'a claim with publicKey undefined',
       should: `throw an error with the message ${expectedMessage}`,
       actual: await getClaimSignature({ ...TheRaven, publicKey: undefined }, PrivateKeyEAP).catch(returnError),
-      expected: new Error(expectedMessage)
+      expected: new Error(expectedMessage),
     })
   }
 
@@ -322,7 +335,7 @@ describe('Claim', async (should: any) => {
         { ...TheRaven, publicKey: '03f0dc475e93105bdc7701b40003200039202ffd4a0789696c78f9b34d5518aef9' },
         PrivateKeyEAP
       ).catch(returnError),
-      expected: new Error(expectedMessage)
+      expected: new Error(expectedMessage),
     })
   }
 
@@ -373,7 +386,7 @@ describe('Claim', async (should: any) => {
 
   {
     const result = await Promise.all(
-      ['', false, null, undefined].map(value => isValidClaim({ ...TheRaven, dateCreated: value }))
+      ['', false, null, undefined].map(value => isValidClaim({ ...TheRaven, created: value }))
     )
 
     assert({
