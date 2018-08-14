10f29892ef380f056ca6b03caf2d2327fbcd38b1
Jesse Wicks <jessewicks@gmail.com>
2018-08-14 12:34:18 -0500

    fix promise handling

---
+++ src/Insight.test.ts
@@ -0,0 +1,82 @@
+/* tslint:disable:no-relative-imports */
+import { describe, Try } from 'riteway'
+
+import { responseNotOk, isNotArray, UnexpectedResponseError } from './Insight'
+
+describe('responseNotOk()', async (should: any) => {
+  const { assert } = should()
+  const createResponse = (ok: boolean = false) => ({
+    ok,
+    text() {
+      return Promise.resolve('text')
+    },
+  })
+
+  {
+    let actual
+
+    await responseNotOk(createResponse(true))
+      .then(res => (actual = 'did not throw'))
+      .catch(e => (actual = 'threw error'))
+
+    assert({
+      given: 'response.ok is true',
+      should: 'should not throw',
+      actual,
+      expected: 'did not throw',
+    })
+  }
+
+  {
+    let actual
+
+    await responseNotOk(createResponse())
+      .then(res => (actual = 'did not throw'))
+      .catch(e => (actual = 'threw error'))
+
+    assert({
+      given: 'response.ok is true',
+      should: 'should throw',
+      actual,
+      expected: 'threw error',
+    })
+  }
+})
+
+describe('isNotArray()', async (should: any) => {
+  const { assert } = should()
+  assert({
+    given: 'array',
+    should: 'should not throw/ return undefined',
+    actual: Try(isNotArray, ['test', 'array']),
+    expected: undefined,
+  })
+
+  assert({
+    given: 'array of undefined values',
+    should: 'should not throw/ return undefined',
+    actual: Try(isNotArray, [undefined, undefined]),
+    expected: undefined,
+  })
+
+  assert({
+    given: 'string',
+    should: 'should throw new UnexpectedResponseError',
+    actual: Try(isNotArray, 'test string'),
+    expected: new UnexpectedResponseError('InsightHelper.getUtxo was expecting server response to be an Array. '),
+  })
+
+  assert({
+    given: 'object',
+    should: 'should throw new UnexpectedResponseError',
+    actual: Try(isNotArray, { test: 'test Object' }),
+    expected: new UnexpectedResponseError('InsightHelper.getUtxo was expecting server response to be an Array. '),
+  })
+
+  assert({
+    given: 'nothing',
+    should: 'should throw new UnexpectedResponseError',
+    actual: Try(isNotArray),
+    expected: new UnexpectedResponseError('InsightHelper.getUtxo was expecting server response to be an Array. '),
+  })
+})
