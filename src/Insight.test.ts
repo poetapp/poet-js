a790f0fe0a8ef185bd5f90b1bc5af73a2ff81878
Jesse Wicks <jessewicks@gmail.com>
2018-08-14 12:16:27 -0500

    use riteway Try

--- src/Insight.test.ts
+++ src/Insight.test.ts
@@ -1,41 +1,22 @@
 /* tslint:disable:no-relative-imports */
-import { describe } from 'riteway'
+import { describe, Try } from 'riteway'
 
 import { throwError, InsightError, BlockHeightOutOfRangeError } from './Insight'
 
 describe('throwError', async (should: any) => {
   const { assert } = should()
 
-  {
-    const error = () => {
-      try {
-        return throwError('random')
-      } catch (e) {
-        return e
-      }
-    }
+  assert({
+    given: 'random string',
+    should: 'throw new InsightError with random string',
+    actual: Try(throwError, 'random'),
+    expected: new InsightError('random'),
+  })
 
-    assert({
-      given: 'random string',
-      should: 'throw new InsightError with random string',
-      actual: error(),
-      expected: new InsightError('random'),
-    })
-  }
-  {
-    const error = () => {
-      try {
-        return throwError('Block height out of range. Code:-8')
-      } catch (e) {
-        return e
-      }
-    }
-
-    assert({
-      given: 'Block height out of range. Code:-8',
-      should: 'throw new BlockHeightOutOfRangeError',
-      actual: error(),
-      expected: new BlockHeightOutOfRangeError(),
-    })
-  }
+  assert({
+    given: 'Block height out of range. Code:-8',
+    should: 'throw new BlockHeightOutOfRangeError',
+    actual: Try(throwError, 'Block height out of range. Code:-8'),
+    expected: new BlockHeightOutOfRangeError(),
+  })
 })
