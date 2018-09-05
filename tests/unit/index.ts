b01f19c5c9fa0ee0d912d381598f62a5d2d75fe8
Geoff Turk <geoff.turk@po.et>
2018-09-05 21:56:18 +0200

    Remove unit test boilerplate

--- tests/unit/index.ts
+++ tests/unit/index.ts
@@ -1,6 +1,3 @@
-// All unit test files should be imported here. For example,
-// import '../../src/foo.test'
-// import '../../src/canary.test'
 /* tslint:disable:no-relative-imports */
 import '../../src/Claim.test'
 import '../../src/Insight.test'
