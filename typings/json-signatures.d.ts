8b5c83f5f9b290454075f6a75b1c3bf4716a6614
Kris Robison <krobison@gmail.com>
2018-09-07 15:42:31 -0700

    Further describe jsonld-signatures type

--- typings/json-signatures.d.ts
+++ typings/json-signatures.d.ts
@@ -1,4 +1,11 @@
 declare module 'jsonld-signatures' {
+  let suites: any
+  let SECURITY_CONTEXT_URL: string
+  interface VerifiedResults {
+    readonly keyResults: ReadonlyArray<any>
+    readonly verified: boolean
+  }
   export function sign(doc: any, signingOptions: any): Promise<any>
-  export function verify(doc: any, verificationOptions: any): Promise<any>
+  export function verify(doc: any, verificationOptions: any): Promise<VerifiedResults>
+  export function use(libraryName: string, library: any): void
 }
