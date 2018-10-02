a7ab315c58183173aaca30b552e5333560d3e997
Kris Robison <krobison@gmail.com>
2018-10-02 15:10:23 -0700

    Let jsonld module assign documentLoader

--- typings/jsonld.d.ts
+++ typings/jsonld.d.ts
@@ -1,6 +1,7 @@
 declare module 'jsonld' {
   interface JsonldFactory {
     canonize: (doc: any) => Promise<string>
+    documentLoader: (url: string, callback: (error: any, data: any) => any) => any
   }
   function factory(): JsonldFactory
 
