74f4a5a1e44b869b1a7f9994ff94bce78b182f26
Kris Robison <krobison@gmail.com>
2018-10-02 15:11:16 -0700

    Set json-signatures module to export the factory

--- typings/json-signatures.d.ts
+++ typings/json-signatures.d.ts
@@ -1,11 +1,18 @@
 declare module 'jsonld-signatures' {
-  let suites: object
-  let SECURITY_CONTEXT_URL: string
   interface VerifiedResults {
     readonly keyResults: ReadonlyArray<any>
     readonly verified: boolean
   }
-  export function sign(doc: any, signingOptions: any): Promise<any>
-  export function verify(doc: any, verificationOptions: any): Promise<VerifiedResults>
-  export function use(libraryName: string, library: any): void
+
+  interface JsonldSignatureFactory {
+    suites: object
+    SECURITY_CONTEXT_URL: string
+    sign: (doc: any, signingOptions: any) => Promise<any>
+    use: (libraryName: string, library: any) => void
+    verify: (doc: any, verificationOptions: any) => Promise<VerifiedResults>
+  }
+
+  function factory(): JsonldSignatureFactory
+
+  export = factory
 }
