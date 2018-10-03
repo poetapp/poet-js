692712cde42bf3d5d65088072fd0da8e88eaa5cf
Kris Robison <krobison@gmail.com>
2018-10-03 16:51:16 -0700

    Remove JsonldHelper Test

--- tests/unit/index.ts
+++ tests/unit/index.ts
@@ -2,5 +2,4 @@
 import '../../src/Claim.test'
 import '../../src/Interfaces.test'
 import '../../src/claim_signer/GetClaimSigner.test'
-import '../../src/claim_signer/JsonLdHelper.test'
 import '../../src/util/KeyHelper.test'
